c18c7f82af63b166e98331909c0662e37a5eb0fe
Ubuntu <luckyboy3214@naver.com>
2023-02-10 13:59:12 +0900

    eit mkvideo

--- mk_video.py
+++ mk_video.py
@@ -5,22 +5,23 @@ from pathlib import Path
 from moviepy.editor import ImageSequenceClip
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--data_path", type=str, 
-                    default="/data/NIA50/50-2/models/DeepFusionMOT/results/nia50_train1st/image", help="image data path")
-parser.add_argument("--output", "-o", type=str, default=None, help="output video path")
-args = parser.parse_args()
 
-if args.output is None:
-    args.output = os.path.join('/'.join(args.data_path.split('/')[:-1]), "video")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", "-d", type=str, help="image data path")
+    parser.add_argument("--ext", "-e", type=str, default="jpg", help="image extension")
+    parser.add_argument("--output", "-o", type=str, default="./", help="output video path")
+    args = parser.parse_args()
 
-if not os.path.exists(args.output):
-    os.makedirs(args.output)
+    if args.output is None:
+        args.output = os.path.join('/'.join(args.data_path.split('/')[:-1]), "video")
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+        
+    # load images
+    images_path = Path(args.data_path)
+    print("collecting images from {}".format(images_path)
+    image_files = sorted([str(p) for p in images_path.rglob(f"**/*.{arg.ext}}")])
+    clip = ImageSequenceClip(image_files, fps=30)
 
-# load images
-images_path = Path(args.data_path)
-image_files = sorted(list(images_path.glob("**/*.jpg")))
-image_files = [str(image_file) for image_file in image_files]
-clip = ImageSequenceClip(image_files, fps=30)
-
-clip.write_videofile("/data/NIA50/50-2/models/DeepFusionMOT/results/nia50_train1st/video/nia50_train1st.mp4")
+    clip.write_videofile(os.path.join(args.output, "video.mp4"), fps=30)
